4c489ed624c86b8e1e01c5717f44257acddb58d3
798295001 <798295001@qq.com>
2024-04-11 14:47:09 +0800

    第二次提交

--- Assets/TestFindTheWay.cs
+++ Assets/TestFindTheWay.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TestFindTheWay : MonoBehaviour
 {
-    public bool  Done = false;
+    public bool  Done = true;
 
     // Start is called before the first frame update
     void Start()
